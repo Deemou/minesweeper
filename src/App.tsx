f25533ffa01a8cda3492c66a88dc568860d8d1f3
Deemou <yepchani@gmail.com>
2023-07-09 23:17:19 +0900

    feat(components): add board

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Board from 'components/board';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/board.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from 'app/hooks';
+import { setBoard, setGameWonStatus } from 'app/slices/boardSlice';
+import generateBoard from 'utils/generateBoard';
+import { ITile } from 'types/boardTypes';
+import Tile from 'components/tile';
+
+export default function Board() {
+  const { boardWidth, boardHeight, bombsCount, board } = useAppSelector(
+    (state) => state.boardSlice
+  );
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const newBoard = generateBoard();
+    dispatch(setBoard(newBoard));
+  }, [boardWidth, boardHeight, bombsCount]);
+
+  useEffect(() => {
+    if (bombsCount !== 0) return;
+    const isAllSafeTilesOpen = board
+      .flat()
+      .every((tile) => tile.isBomb || tile.isOpen);
+
+    if (isAllSafeTilesOpen) dispatch(setGameWonStatus({ status: true }));
+  }, [board, bombsCount]);
+
+  return (
+    <div>
+      {board.map((row) => {
+        return row.map((tile: ITile) => {
+          return <Tile key={`${tile.x}.${tile.y}`} {...tile} />;
+        });
+      })}
+    </div>
+  );
+}
