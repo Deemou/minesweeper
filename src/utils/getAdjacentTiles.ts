a807b400219bdc60e9683e3f3418c938492cef8c
Deemou <yepchani@gmail.com>
2023-07-09 20:23:14 +0900

    feat: add getAdjacentTiles

---
+++ src/utils/getAdjacentTiles.ts
@@ -0,0 +1,16 @@
+import { useAppSelector } from 'app/hooks';
+import { ITile } from 'types/boardTypes';
+
+export default function getAdjacentTiles(x: number, y: number): ITile[] {
+  const { board } = useAppSelector((state) => state.boardSlice);
+  const tiles: ITile[] = [];
+
+  for (let xOffset = -1; xOffset <= 1; xOffset++) {
+    for (let yOffset = -1; yOffset <= 1; yOffset++) {
+      const tile: ITile = board[x + xOffset]?.[y + yOffset];
+
+      if (tile) tiles.push(tile);
+    }
+  }
+  return tiles;
+}
