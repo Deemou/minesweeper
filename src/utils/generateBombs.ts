ca8f65d38831278f335a343bae3388ea33e07f86
Deemou <yepchani@gmail.com>
2023-07-09 18:33:25 +0900

    feat(util): add generateBombs

---
+++ src/utils/generateBombs.ts
@@ -0,0 +1,21 @@
+import { ICoord } from 'types/boardTypes';
+import getRandomNum from './getRandomNum';
+import { useAppSelector } from 'app/hooks';
+
+export function generateBombs(): ICoord[] {
+  const { boardWidth, boardHeight, bombsCount } = useAppSelector(
+    (state) => state.boardSlice
+  );
+  const positions = new Set<ICoord>();
+
+  while (positions.size < bombsCount) {
+    const bombPosition: ICoord = {
+      x: getRandomNum(boardWidth),
+      y: getRandomNum(boardHeight)
+    };
+
+    positions.add(bombPosition);
+  }
+
+  return [...positions];
+}
